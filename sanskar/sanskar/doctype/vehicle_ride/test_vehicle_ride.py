d13c0a9daad02c5f9c27f7db670e3258b0f9d880
Jeevahari <jeeva.hari@pravaig.com>
2024-09-25 14:31:53 +0530

    Test

---
+++ sanskar/sanskar/doctype/vehicle_ride/test_vehicle_ride.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, jeeva and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVehicleRide(FrappeTestCase):
+	pass
---
+++ sanskar/sanskar/doctype/vehicle_ride/vehicle_ride.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, jeeva and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Vehicle Ride", {
+// 	refresh(frm) {
+
+// 	},
+// });
